a1ecff33cbc5ce5a6fa6a4d2fb72523e199d6955
Sellia-Stray <sellialisa@gmail.com>
2024-10-17 21:40:28 +0300

    Add files via upload

---
+++ strelka.s
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
+<!-- Creator: CorelDRAW 2020 (64-Bit) -->
+<svg xmlns="http://www.w3.org/2000/svg" xml:space="preserve" width="2.0446mm" height="1.4384mm" version="1.1" style="shape-rendering:geometricPrecision; text-rendering:geometricPrecision; image-rendering:optimizeQuality; fill-rule:evenodd; clip-rule:evenodd"
+viewBox="0 0 204.46 143.84"
+ xmlns:xlink="http://www.w3.org/1999/xlink"
+ xmlns:xodm="http://www.corel.com/coreldraw/odm/2003">
+ <defs>
+  <style type="text/css">
+   <![CDATA[
+    .fil0 {fill:silver;fill-rule:nonzero}
+   ]]>
+  </style>
+ </defs>
+ <g id="Слой_x0020_1">
+  <metadata id="CorelCorpID_0Corel-Layer"/>
+  <path class="fil0" d="M74.07 138.62l-75.79 -90.95c-3.22,-3.87 -5.27,-8.57 -5.92,-13.55 -0.64,-4.99 0.15,-10.05 2.28,-14.61 2.14,-4.55 5.52,-8.4 9.76,-11.1 4.24,-2.7 9.17,-4.13 14.2,-4.14l151.59 0c5.03,0.01 9.95,1.44 14.19,4.14 4.25,2.7 7.63,6.55 9.76,11.1 2.13,4.56 2.93,9.62 2.28,14.61 -0.64,4.98 -2.69,9.68 -5.91,13.55l-75.8 90.95c-2.48,2.98 -5.59,5.37 -9.1,7.02 -3.51,1.64 -7.34,2.49 -11.22,2.49 -3.88,0 -7.71,-0.85 -11.22,-2.49 -3.51,-1.65 -6.62,-4.04 -9.1,-7.02z"/>
+ </g>
+</svg>
